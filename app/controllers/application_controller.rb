d72ca859a9e1f069394a5384d47e63ab06fe4f10
Chris Benedict <chrisjbenedict@gmail.com>
2018-12-12 12:11:41 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,56 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    Rack::MethodOverride
   end
 
   get '/' do
   end
+
+  # new - just form, not submitting
+  get '/articles/new' do
+    erb :new
+  end
+
+  # index
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  # show
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  # create
+  post '/articles' do
+    @article = Article.create(params)
+    redirect "/articles/#{@article.id}"
+  end
+
+  # edit --- edit form
+  get '/articles/:id/edit' do
+    # model?
+    @article = Article.find(params[:id])
+    # resp?
+    erb :edit
+  end
+
+  # update
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
+    @article.save
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect "/articles"
+  end
+
+
 end
